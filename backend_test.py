7d9fb99a8d836c944b2d93d7400259b51894f74e
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-05-07 02:22:39 +0800

    winRatio_v1

--- backend_test.py
+++ backend_test.py
@@ -38,11 +38,10 @@ def printFn(name, value:float, percentage = True, showAllDigits = False):
 
 
 def computeROI (data:list, cost:float):
-    netProfit = 0
+    netProfit = 0.0
     
     for trade in data:
-        netProfit += trade['fee']
-        netProfit += trade['fillPnl']
+        netProfit += trade['fee'] + trade['fillPnl']
 
     return netProfit / cost 
 
@@ -70,13 +69,18 @@ def computeMDD (data:list, cost:float):
         
     return MDD 
 
-def computeOddsRatio (data:list, cost:float):
-    pass
+def computeOddsRatio (data:list):
+    winRate  = computeWinRate(data)
+    lossRate = 1 - winRate
+    
+    return winRate / lossRate
+            
 
 def computeProfitFactor (data:list):
     grossProfit = 0.0
     grossLoss = 0.0
-    # I didn't consider fee in this case, because I think ProfitFactor only focus on the performance of your strategy.
+    # | I didn't consider fee in this case, 
+    # | because I think Profit Factor only focus on the performance of our strategies.
     
     for trade in data:
         gross = trade['fillPnl']
@@ -86,48 +90,67 @@ def computeProfitFactor (data:list):
             grossProfit += gross
         else:
             grossLoss -= gross
+
             
     return grossProfit / grossLoss
 
 def computeSharpeRatio (data:list, cost:float, riskFreeRate:float):
     ROI = computeROI(data, cost)
     
+    # | this part I only consider std of ETH 
+    # | since the problem statement only states that:
+    # | " `fillSz` 值為合約張數，每張合約大小為 0.01 ETH "
+    # | which means we are more focus on ETH
+    
     prices_ETH = list(map(
         lambda x:x['fillPx'],
         [ETH for ETH in data if ETH['instId'] == "ETH-USDT-SWAP"])
     )
     
-    prices_BTC = list(map(
-        lambda x:x['fillPx'],
-        [ETH for ETH in data if ETH['instId'] == "BTC-USDT-SWAP"])
-    )
+    # prices_BTC = list(map(
+    #     lambda x:x['fillPx'],
+    #     [ETH for ETH in data if ETH['instId'] == "BTC-USDT-SWAP"])
+    # )
     
     STD_ETH = statistics.stdev(prices_ETH)
-    STD_BTC = statistics.stdev(prices_BTC)
+    # STD_BTC = statistics.stdev(prices_BTC)
     
-    STD = math.sqrt(STD_ETH * STD_BTC) 
+    # q_ETH = sum(prices_ETH)
     
-    return (ROI - riskFreeRate) / STD
+    # q_BTC = sum(prices_BTC)
+    
+    # q_total = q_ETH + q_BTC
+    # STD = ((STD_ETH * q_ETH) + (STD_BTC * q_BTC)) / q_total
+    
+    
+    
+    return (ROI - riskFreeRate) / STD_ETH
         
 if __name__ == '__main__':
+    
     code, msg, data = loadData('trades.json')
     
+    # parameters
     cost = 8000
     data = preprocess(data)
-    riskFreeRate = 0.02
+    riskFreeRate = 0.02 # assumption
 
 
+    # calculation
     ROI     = computeROI(data, cost)
     winrate = computeWinRate(data)
     MDD     = computeMDD(data, cost)
     
+    oddsRatio    = computeOddsRatio(data)
     ProfitFactor = computeProfitFactor(data)
     sharpRatio   = computeSharpeRatio(data, cost, riskFreeRate)
     
+    
+    # print
     printFn('ROI', ROI)
     printFn('winrate', winrate)
     printFn('MDD', MDD)
-    
+    printFn('Odds Ratio', oddsRatio)
     printFn('Profit Factor', ProfitFactor, False)
     printFn('Sharp Ratio', sharpRatio, False, True)
 
